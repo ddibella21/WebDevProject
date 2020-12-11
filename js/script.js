0c884e65ca0779e9fcdffe34b307a36052145a99
ddibella21 <73658940+ddibella21@users.noreply.github.com>
2020-12-11 13:58:59 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 var counter=0;
+var running = false;
 function jump(){
+    running = true;
     if(character.classList == "animate"){return}
     character.classList.add("animate");
     setTimeout(function(){
@@ -11,11 +13,12 @@ function jump(){
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130 && running){
         block.style.animation = "none";
         alert("Game Over. score: "+Math.floor(counter/100));
         counter=0;
         block.style.animation = "block 1s infinite linear";
+        running = false;
     }else{
         counter++;
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
